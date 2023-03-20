8b86f4c8140d42da8046dbfe8256762823ca8a38
= <=>
2023-03-20 20:13:52 +0400

    fix: data flow

--- src/components/card/StatsCard.js
+++ src/components/card/StatsCard.js
@@ -4,15 +4,14 @@ import { Repos, Followers, Following } from '@/icons/icons'
 import styles from './StatsCard.module.scss'
 
 const StatsCard = ({ data }) => {
-    console.log(data);
     return (
         <section>
             <div className="container">
                 <div className={styles['stats-container']}>
-                    <StatsCardItem icon={<Repos />} text='repos' num={data.public_repos} />
-                    <StatsCardItem icon={<Followers />} text='followers' num={data.followers} />
-                    <StatsCardItem icon={<Following />} text='following' num={data.following} />
-                    <StatsCardItem icon={<Repos />} text='gists' num={data.public_gists} />
+                    <StatsCardItem icon={<Repos />} text='repos' num={data?.public_repos} />
+                    <StatsCardItem icon={<Followers />} text='followers' num={data?.followers} />
+                    <StatsCardItem icon={<Following />} text='following' num={data?.following} />
+                    <StatsCardItem icon={<Repos />} text='gists' num={data?.public_gists} />
                 </div>
 
             </div>
--- src/components/profile/UserCard.js
+++ src/components/profile/UserCard.js
@@ -5,47 +5,50 @@ import styles from './Profile.module.scss';
 import { Company, LinkIcon, Location } from '@/icons/icons';
 
 const UserCard = ({ data }) => {
+    console.log(data)
     return (
         <div className={`${styles.card}`}>
             <div className={styles.user}>
                 <article>
                     <header>
-                        <img
-                            src={data.avatar_url}
-                            alt={data.name}
-                        />
+                        <img src={data?.avatar_url} alt={data?.name} />
                         <div>
-                            <h4>{data.name}</h4>
-                            <p>@{data.login}</p>
+                            <h4>{data?.name}</h4>
+                            <p>@{data?.login}</p>
                         </div>
                         <a
                             className={styles['follow-link']}
-                            href={data.html_url}
+                            href={data?.html_url}
                         >
                             follow
                         </a>
                     </header>
-                    <p className={styles.bio}>{data.bio}</p>
+                    <p className={styles.bio}>{data?.bio}</p>
                     <div className={styles['user-info']}>
-                        {data.company && <p>
-                            <Company />
-                            {data.company}
-                        </p>}
-                        {data.location && <p>
-                            <Location />
-                            {data.location}
-                        </p>}
-                        {data.blog && <p>
-                            <a href={`https://www.${data.blog}`}>
-                                <LinkIcon />
-                                {data.blog}
-                            </a>
-                        </p>}
-
+                        {data?.company && (
+                            <p>
+                                <Company />
+                                {data?.company}
+                            </p>
+                        )}
+                        {data?.location && (
+                            <p>
+                                <Location />
+                                {data?.location}
+                            </p>
+                        )}
+                        {data?.blog && (
+                            <p>
+                                <a href={`https://www.${data?.blog}`}>
+                                    <LinkIcon />
+                                    {data?.blog}
+                                </a>
+                            </p>
+                        )}
                     </div>
                 </article>
-            </div >
-        </div >
+            </div>
+        </div>
     );
 };
 
