44f5af75f599d414c38263b5497732e59bf32829
Shadman <shadman.alizada@gmail.com>
2024-01-06 23:27:19 +0400

    refactor: recreate Dashboard comp

--- src/container/Dashboard/index.tsx
+++ src/container/Dashboard/index.tsx
@@ -1,36 +1,46 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-import StatsCard from '@/components/card/StatsCard';
-import Search from '@/components/search/Search';
-import Profile from '@/components/profile/Profile';
+import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+import StatsCard from "@/components/card/StatsCard";
+import Profile from "@/components/profile/User";
 const NextFusionCharts = dynamic(
-    () => import('@/components/fusioncharts/NextFusionChart'),
-    { ssr: false }
+  () => import("@/components/fusioncharts/NextFusionChart"),
+  { ssr: false },
 );
 import {
-    useGetGithubUserByNameQuery,
-    useGetGithubUserByFollowersQuery,
-    useGetGithubUserByReposQuery,
-} from '@/redux/services/githubApi';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
+  useGetGithubUserByNameQuery,
+  useGetGithubUserReposQuery,
+} from "@/redux/services/githubApi";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import Loading from "@/components/common/Loading";
+import Sidebar from "@/components/Sidebar";
+import { setUser } from "@/redux/features/userSlice";
+import Followers from "@/components/followers";
 
 export default function Dashboard() {
-    const { searchUser } = useSelector((state: RootState) => state.users);
-    const { data, isError } = useGetGithubUserByNameQuery(searchUser);
-    const { data: reposData, isLoading } =
-        useGetGithubUserByReposQuery(searchUser);
+  const username = useSelector((state: RootState) => state.user.username);
+  const dispatch = useDispatch();
+  const { data, isLoading, isError, isSuccess } =
+    useGetGithubUserByNameQuery(username);
+  const { data: reposData } = useGetGithubUserReposQuery(username);
 
-    return (
-        <>
-            {!isLoading && (
-                <>
-                    <Search isError={isError} />
-                    <StatsCard data={data} />
-                    <Profile data={data} />
-                    <NextFusionCharts reposData={reposData} />
-                </>
-            )}
-        </>
-    );
+  useEffect(() => {
+    dispatch(setUser(data));
+  }, [isSuccess, dispatch, data]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="grid h-full max-h-[900px] grid-cols-[1fr_5fr] overflow-y-scroll">
+      <Sidebar />
+      <div className="p-8">
+        <Followers />
+        <Profile data={data} />
+        <StatsCard data={data} />
+        <NextFusionCharts reposData={reposData} />
+      </div>
+    </div>
+  );
 }
