862cddf339aa1245f7e826ba73161dcfe151e344
Shadman <shadman.alizada@gmail.com>
2024-01-08 22:09:33 +0400

    refactor: prepared app comp for nested routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
-import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '@/redux/store';
-import Layout from './_layout';
-import '@/styles/globals.scss';
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Provider store={store}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </Provider>
-    );
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page: ReactElement) => page);
+
+  return (
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
 }
