71b2aa814dc982a76047d0d7a01604e1562f6fe7
Shadman <shadman.alizada@gmail.com>
2024-01-13 23:26:42 +0400

    refactor: removed Menu component, used Search input component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Logo from "./Logo";
-import Menu from "./Menu";
 import ProfileInfo from "./ProfileInfo";
+import Search from "../Search";
+import { useGetRateLimitQuery } from "@/redux/services/githubApi";
 
 export default function Navbar() {
+  const { data: rateLimit, refetch } = useGetRateLimitQuery({});
+  console.log(rateLimit);
   return (
-    <nav className="flex w-full items-center justify-between">
+    <nav className="relative flex w-full items-center justify-between">
       <Logo />
-      <Menu />
-      <ProfileInfo />
+      <Search refetch={refetch} rateLimit={rateLimit} />
+      <ProfileInfo rateLimit={rateLimit} />
     </nav>
   );
 }
