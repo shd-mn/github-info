662ebe292aa075e968e619d5b9a750156ceb4b45
Shadman <shadman.alizada@gmail.com>
2024-01-25 23:21:27 +0400

    feat: added handleSidebarShow fn toggle sidebar

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,6 +1,25 @@
-import Link from 'next/link';
-import React from 'react';
-
+import React from "react";
+import Link from "next/link";
+import { IoMenu } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { toggleSidebar } from "@/redux/features/mainSlice";
 export default function Logo() {
-    return <Link href="/">Logo</Link>;
+  const { isSidebarOpen } = useSelector((state: RootState) => state.main);
+  console.log(isSidebarOpen)
+  const dispatch = useDispatch();
+
+  const handleSidebarShow = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <div className="flex items-center gap-8">
+      <Link href="/" className="text-lg">
+        GithubInfo
+      </Link>
+      <button type="button" onClick={handleSidebarShow}>
+        <IoMenu size={24} />
+      </button>
+    </div>
+  );
 }
